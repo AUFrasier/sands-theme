5831711a807cbbe1b6a69a84d02a6e04d3e8e82e
Denisa Lalikova <denisa.lalikova@artunlimiteusa.com>
2020-09-09 16:26:07 -0500

    newest chnages

--- assets/js/src/front-page.js
+++ assets/js/src/front-page.js
@@ -1,15 +1,22 @@
-var swiper = new Swiper('.swiper-container', {
-    pagination: {
-      el: '.swiper-pagination',
-      dynamicBullets: true,
-    },
-  });
+
+  /*
   var swiperTestimonials = new Swiper('.swiper-container-testimonials', {
     pagination: {
       el: '.swiper-pagination-testimonials',
       dynamicBullets: true,
     },
   });
-
-
+*/
+  var swiperTestimonials = new Swiper('.swiper-container-testimonials', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination-testimonials',
+      clickable: true,
+    },
+  });
 
